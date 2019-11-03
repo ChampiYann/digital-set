444b5fedc4388126c55c7f9d0751b58e311074b7
Yann Rosema <yann.rosema@hotmail.com>
2019-11-03 14:50:52 +0100

    added contour counting using the hierarchy

--- src/callCamera.py
+++ src/callCamera.py
@@ -7,42 +7,43 @@ cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # frame = cv2.imread('.\set.jpg')
 
-    # frame = cv2.GaussianBlur(frame,(5,5),0)
+    # Convert to grayscale
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray,thresh1 = cv2.threshold(gray,130,255,cv2.THRESH_BINARY)
-
+    # Make a binary version of it
+    ret3,thresh1 = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
     gray = np.float32(thresh1)
-
+    # find the contours
     contours,hirerarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-
-    # dst = cv2.cornerHarris(gray,4,5,0.04)
-
-    #result is dilated for marking the corners, not important
-    # dst = cv2.dilate(dst,None)
-
-    # Threshold for an optimal value, it may vary depending on the image.
-    # frame[dst>0.01*dst.max()]=[0,0,255]
-
-    # Display the resulting frame
-    topLevel = [i for i, x in enumerate(hirerarchy[0]) if x[3]==-1]
-    rects = [cv2.minAreaRect(contours[i]) for i in topLevel]
+    # Make rectangles of the contours
+    rects = [cv2.minAreaRect(contour) for contour in contours]
     boxes = [cv2.boxPoints(rect) for rect in rects]
     boxes = np.int0(boxes)
     areas = [cv2.contourArea(box) for box in boxes]
-    # countChildren = [hirerarchy for i in topLevel]
 
-    # for cnts in contours:
-    cv2.drawContours(frame,[boxes[i] for i in range(len(areas)) if areas[i] >= 3000],-1,(255,0,255),2)
+    # Scan rectangles for for the ones looking like cards
+    cardIDs = [i for i in range(len(contours)) if areas[i] >= 3000 and hirerarchy[0][i][3] == -1]
+    cards = boxes[cardIDs]
+
+    numElem = [len([hire for hire in hirerarchy[0] if hire[3] == j]) for j in cardIDs]
+
+    for i in range(len(cardIDs)):
+        M = cv2.moments(cards[i])
+
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        cv2.putText(frame,str(numElem[i]),(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,0,255),2)
+
+    cv2.drawContours(frame,cards,-1,255,2)
 
     cv2.imshow('frame',frame)
     cv2.imshow('gray',gray)
     if cv2.waitKey(1) & 0xFF == 27:
         break
+    # cv2.waitKey()
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-
